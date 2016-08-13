d1f0034321372d302faac7e0e2da32ae701a5bc9
Александр <britishop@gmail.com>
2016-08-13 16:22:17 +0300

    add Frontend class

--- src/main/java/Frontend.java
+++ src/main/java/Frontend.java
@@ -4,7 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Created by Александр on 13.08.2016.
+ * Created by Alexandr on 13.08.2016.
  */
 public class Frontend extends HttpServlet {
     private String login = "";
