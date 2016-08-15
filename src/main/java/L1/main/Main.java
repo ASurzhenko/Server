dc01d38d9dbece697b89625f1874f29ad5acdc4b
Александр <britishop@gmail.com>
2016-08-15 23:25:35 +0300

    update

---
+++ src/main/java/L1/main/Main.java
@@ -0,0 +1,24 @@
+package L1.main;
+
+import L1.servlets.AllRequestsServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ * Created by Александр on 15.08.2016.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
+}
